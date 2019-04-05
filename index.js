0678add2678885ef40766bfde5e0d0d557d5ff60
Dicino <738176@corpaa.aa.com>
2019-04-05 14:20:32 -0700

    Added SignalR subscription (connection, updated flights) and cleaned up

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const cache = new apolloboost.InMemoryCache();
-const link = new apolloboost.HttpLink({uri: 'https://gql-flighthub.azurewebsites.net/api/http-entry'});
-const client = new apolloboost.ApolloClient({cache, link});
+const link = new apolloboost.HttpLink({ uri: 'https://gql-flighthub.azurewebsites.net/api/http-entry' });
+const client = new apolloboost.ApolloClient({ cache, link });
 const gql = apolloboost.gql;
 const mom = moment;
+const apiBaseUrl = 'http://gql-flighthub.azurewebsites.net';
+const axiosConfig = {}
 
+function startConnection(connection) {
+    console.log('Starting connection to SignalR...')
+    connection.start()
+        .then(function () { console.log('SignalR Connected!') })
+        .catch(function (err) {
+            console.error(err)
+            setTimeout(function () { startConnection(connection) }, 2000)
+        })
+}
+
+function getConnectionInfo() {
+    console.log('Get SignalR connection info');
+    return axios.post(`${apiBaseUrl}/api/SignalRInfo`, null, axiosConfig)
+        .then(function (resp) { return resp.data })
+        .catch(function () { return {} })
+}
 
 console.log('QUERY http-entry for flight stats data');
 
 client.query({
     query: gql
-    `{
+        `{
         getFlightsArrivingToday{
             flightId
             flightNumber
@@ -23,38 +41,41 @@ client.query({
             status
        } 
     }`
-  }).then(result => BuildFlights(result)).catch(error => console.error(error));
+}).then(result => {
+    BuildFlights(result);
+    getConnectionInfo().then(function (info) {
+        let accessToken = info.accessToken
+        const options = {
+            accessTokenFactory: function () {
+                if (accessToken) {
+                    const _accessToken = accessToken
+                    accessToken = null
+                    return _accessToken
+                } else {
+                    return getConnectionInfo().then(function (info) {
+                        return info.accessToken
+                    })
+                }
+            }
+        }
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(info.url, options)
+            .build()
 
 
-  function BuildFlights(data) {
-    console.log('BUILD FLIGHTS DATA');
-    
-    var dataArray = data.data.getFlightsArrivingToday;
+        connection.on('flightUpdated', flightUpdated)
 
-    for(var i = 0; i < dataArray.length; i++)
-    {
-        const flight = dataArray[i];
-        const flightNumber = flight.flightNumber;
-        const departureStation = flight.departureAirport.iata;
-        const departureTime = mom(flight.departureDate.dateLocal).format("hh:mm a");
-        const arrivalTime = mom(flight.arrivalDate.dateLocal).format("hh:mm a");
-        const arrivalTerminal = flight.airportResources && flight.airportResources.arrivalTerminal || " ";
-        const arrivalGate = flight.airportResources && flight.airportResources.arrivalGate || " ";
-        const status = hashtable[flight.status];
+        connection.onclose(function () {
+            console.log('SignalR disconnected')
+            setTimeout(function () { startConnection(connection) }, 2000)
+        })
 
-        $("#tblFlights").append("<tr id=" + flight.flightId + "><td>" +
-        flightNumber + "</td><td>" +
-        departureStation + "</td><td>" +
-        departureTime + "</td><td>" +  
-        arrivalTime + "</td><td>"  +
-        arrivalTerminal + " " + 
-        arrivalGate + "</td><td>" +
-        status + "</td></tr>");
-    }
+        startConnection(connection);
 
-    console.log('FLIGHTS DATA BUILT');
-    
-}
+    });
+}).catch(error => console.error(error));
+
 
 var hashtable = {};
 hashtable['A'] = 'Active';
@@ -66,42 +87,68 @@ hashtable['R'] = 'Redirected';
 hashtable['S'] = 'Scheduled';
 hashtable['U'] = 'Unknown';
 
-
-/*
-//this asks for data from the server to build out the data table
-
-var socket = io();
-socket.on('onDataChanged', UpdateFlight);
-
-getFlights();
-
-function UpdateFlight(data) {
-    var id = $('#' + data.flightId);
-    id.empty();
-    id.append("<td>" + data.flightNumber +
-        "</td><td>" + data.departingAirport +
-        "</td><td>" + data.departureTime +
-        "</td><td>" + data.arrivalTime +
-        "</td><td>" + data.terminal + data.gate +
-        "</td><td>" + hashtable[data.status] + "</td>");
-}
-
 function BuildFlights(data) {
-    $.each(data, function (index, item) {
-        $("#tblFlights").append("<tr id=" + item.flightId + "><td>" +
-                                            item.flightNumber + "</td><td>" +
-                                            item.departingAirport + "</td><td>" +
-                                            item.departureTime +  "</td><td>" +
-                                            item.arrivalTime +  "</td><td>" +
-                                            item.terminal + item.gate + "</td><td>" +
-                                            hashtable[item.status] +
-                                "</td></tr>");
-    });
+    console.log('Building flights');
+
+    var dataArray = data.data.getFlightsArrivingToday;
+
+    for (var i = 0; i < dataArray.length; i++) {
+        const flight = dataArray[i];
+        const arrivalTerminal = flight.airportResources && flight.airportResources.arrivalTerminal || " ";
+        const arrivalGate = flight.airportResources && flight.airportResources.arrivalGate || " ";
+
+        $("#tblFlights").append(
+            "<tr id=" + flight.flightId + 
+            "><td>" + flight.flightNumber + 
+            "</td><td>" + flight.departureAirport.iata + 
+            "</td><td>" + mom(flight.departureDate.dateLocal).format("hh:mm a") + 
+            "</td><td>" + mom(flight.arrivalDate.dateLocal).format("hh:mm a") + 
+            "</td><td>" + arrivalTerminal + 
+            " " + arrivalGate + 
+            "</td><td>" + hashtable[flight.status] + 
+            "</td></tr>");
+
+    }
+
+    console.log('Flight table built!');
 }
 
-function getFlights() {
-    $.get('http://localhost:3000/api/flights', (data) => {
-        BuildFlights(data);
-    })
-}
-*/
+function flightUpdated(updatedFlight) {
+   console.log("SignalR - flight change found");
+   const flightRow = document.getElementById(updatedFlight.$v.flightId.$v);
+
+    if(flightRow) {
+        console.log("change - updating flight...");
+        var id = $('#' + updatedFlight.$v.flightId.$v);
+        id.empty();
+        buildUpdatedRow(id, updatedFlight, false);
+    }
+    else {
+        console.log("change - adding flight...");
+        buildUpdatedRow($("#tblFlights"), updatedFlight, true);
+    }
+    console.log("Update complete");
+  }
+
+  function buildUpdatedRow(tableOrRow, updatedFlight, newFlight)
+  {
+      const arrivalTerminal = updatedFlight.$v.airportResources && updatedFlight.$v.airportResources.$v.arrivalTerminal.$v || " ";
+      const arrivalGate = updatedFlight.$v.airportResources && updatedFlight.$v.airportResources.$v.arrivalGate.$v|| " ";
+      var start = "";
+      var end = "";
+      
+      if (newFlight) {
+        start = "<tr id=" + updatedFlight.$v.flightId.$v + ">";
+        end = "</tr>";
+      }
+
+      tableOrRow.append( start + 
+        "<td>" + updatedFlight.$v.flightNumber.$v +
+        "</td><td>" + updatedFlight.$v.departureAirport.$v.iata.$v +
+        "</td><td>" + mom(updatedFlight.$v.departureDate.$v.dateLocal.$v).format("hh:mm a") +
+        "</td><td>" + mom(updatedFlight.$v.arrivalDate.$v.dateLocal.$v).format("hh:mm a") +
+        "</td><td>" + arrivalTerminal + 
+        " " + arrivalGate +
+        "</td><td>" + hashtable[updatedFlight.$v.status.$v] + 
+        "</td>" + end);
+  }
